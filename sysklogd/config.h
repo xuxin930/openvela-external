17c53a92618401f2bc81c2e3a39679950ff88697
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-09-06 14:39:38 +0800

    external/sysklogd: Merge Submake.mk to Makefile
    Change-Id: Ied83d306244fa91c05d33e2049255ce9af4cb43c

---
+++ sysklogd/config.h
@@ -0,0 +1,95 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `fork' function. */
+/* #undef HAVE_FORK */
+
+/* Define to 1 if you have the `setsid' function. */
+/* #undef HAVE_SETSID */
+
+/* Define to 1 if you have the `getprogname' function. */
+#define HAVE_GETPROGNAME 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `pidfile' function. */
+/* #undef HAVE_PIDFILE */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_STRLCAT 1
+
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_STRLCPY 1
+
+/* Define to 1 if you have the `strtobytes' function. */
+/* #undef HAVE_STRTOBYTES */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `utimensat' function. */
+/* #undef HAVE_UTIMENSAT */
+
+/* Define to 1 if you have the <utmp.h> header file. */
+/* #undef HAVE_UTMP_H */
+
+/* Retry delay for sending to remote syslog servers, default: 180 */
+/* #undef INET_SUSPEND_TIME */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "sysklogd"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/troglobit/sysklogd/issues"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "sysklogd"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "sysklogd 2.3.0-beta1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sysklogd"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/troglobit/sysklogd"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.3.0-beta1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.3.0-beta1"
