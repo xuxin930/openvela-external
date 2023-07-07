9513c78134470993a587ec99acf48339aac8eeca
yangyalei <yangyalei@xiaomi.com>
2023-07-07 17:20:56 +0800

    kmgk: use prebuilt wamrc global
    Change-Id: I5bc5e944b1ebbb95344e9b9b3b7aa19f545f22ad

--- kmgk/build_wasm_keystore.sh
+++ kmgk/build_wasm_keystore.sh
@@ -1,9 +1,10 @@
 MITEE_IOT_DIR=../../vendor/xiaomi/mitee_iot/
+WAMRC=../../prebuilts/clang/linux/wasm/wamrc
 rm ./kmgk/keymaster/ta/*.wo
 rm ${MITEE_IOT_DIR}/tee/lib/libutee/wasm_ta_framework.wo
 rm 88A89F14B906EF4EBD96940EFFDABEF4
 rm 88A89F14B906EF4EBD96940EFFDABEF4.aot
 
 make -f Makefile-wasm
-${MITEE_IOT_DIR}/tee/wasm/wamrc --target=thumbv8m.main --target-abi=gnueabihf --cpu=cortex-m55 --enable-indirect-mode --disable-llvm-intrinsics -o 88A89F14B906EF4EBD96940EFFDABEF4.aot 88A89F14B906EF4EBD96940EFFDABEF4
+${WAMRC} --target=thumbv8m.main --target-abi=gnueabihf --cpu=cortex-m55 --enable-indirect-mode --disable-llvm-intrinsics --enable-builtin-intrinsics=i64.common -o 88A89F14B906EF4EBD96940EFFDABEF4.aot 88A89F14B906EF4EBD96940EFFDABEF4
 cp 88A89F14B906EF4EBD96940EFFDABEF4.aot ../../vendor/bes/boards/best1600_ep/src/etc/ta/88A89F14B906EF4EBD96940EFFDABEF4
