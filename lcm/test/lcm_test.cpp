f6de64f4810d62864863c876de3da320548e7fac
cuiziwei <cuiziwei@xiaomi.com>
2024-05-14 18:18:46 +0800

    lcm:Move lcm_export.h to the upper-level folder.
    Change-Id: I48f3ab1776357d91400f2f22fc593fa4a2d57393

--- lcm/test/lcm_test.cpp
+++ lcm/test/lcm_test.cpp
@@ -5,10 +5,6 @@
 #include "test/c/memq_test.cpp"
 #include "test/c/udpm_test.cpp"
 
-#include "test/cpp/client.cpp"
-#include "test/cpp/common.cpp"
-#include "test/cpp/memq_test.cpp"
-
 extern "C" int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
