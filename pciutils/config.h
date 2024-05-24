bfd28d68a94c50c49bb32c450d9d4c61f7812784
lipengfei28 <lipengfei28@xiaomi.com>
2024-05-24 15:36:16 +0800

    pciutils: add pciutils to nuttx
    Change-Id: I7f18e0287cf86f1200c53f2b603306a614f25627

---
+++ pciutils/config.h
@@ -0,0 +1,12 @@
+#include <sys/endian.h>
+
+#define PCI_CONFIG_H
+#define PCI_HAVE_64BIT_ADDRESS
+#define PCI_IDS "pci.ids.gz"
+#define PCILIB_VERSION "3.12.0"
+#define PCI_HAVE_PM_OBSD_DEVICE
+#define PCI_HAVE_PM_DUMP
+#define PCI_PATH_OBSD_DEVICE "/dev/pci"
+#define PCI_PATH_IDS_DIR "/etc"
+#define xmalloc pciutils_xmalloc
+#define xrealloc pciutils_xrealloc
